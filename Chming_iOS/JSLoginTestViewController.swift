2581afde75a3f3657ad13245f48a2fc18bfc8862
leejaesung <blackturtle2@gmail.com>
2017-08-02 21:53:10 +0900

    Developed data structure and Tested login process
    데이터 구조 설계 중이며, 로그인 메소드의 대략적인 샘플을 구상중입니다.

--- Chming_iOS/JSLoginTestViewController.swift
+++ Chming_iOS/JSLoginTestViewController.swift
@@ -27,16 +27,17 @@ class JSLoginTestViewController: UIViewController {
     
     // MARK: login start //
     @IBAction func btnSignUpTest(_ sender:UIButton) {
-        let tempMember = Member(pkUser: number, email: "blackturtle2@gmail.com", password: "123456", name: "Leejaesung", gender: .man, birthYear: 1988, birthMonth: 10, birthDay: 19, address: "인천시 계양구 작전1동", profileImageUrl: "http://www.google.com")
+        let tempMember = Member(pkUser: number, email: "blackturtle2@gmail.com", password: "123456", name: "Leejaesung", gender: .man, birthYear: 1988, birthMonth: 10, birthDay: 19, address: "인천시 계양구 작전1동", profileImageUrl: "http://www.google.com", interest: .축구)
         let signUpOkay = DataCenter.sharedInstance.signUpWith(member: tempMember)
         
         number = number + 1
         print("/////number: ", number)
         print(signUpOkay)
+        
     }
     
     @IBAction func btnSignInTest(_ sender:UIButton) {
-        let signInOkay = DataCenter.sharedInstance.signInWith(memberEmail: "blackturtle2@gmail.com", memberPassword: "123456")
+        let signInOkay = DataCenter.sharedInstance.signInWith(memberEmail: "chming@gmail.com", memberPassword: "123456")
         
         print(signInOkay)
     }
