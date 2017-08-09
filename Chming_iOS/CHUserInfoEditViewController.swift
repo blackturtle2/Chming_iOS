b8c9936c7bd3d76de3f65cc6f11031b7f0145b5d
EdCLee <llacovoc@gmail.com>
2017-08-09 17:06:34 +0900

    Create few VC for signin/ signup

---
+++ Chming_iOS/CHUserInfoEditViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CHUserInfoEditViewController.swift
+//  Chming_iOS
+//
+//  Created by CLEE on 09/08/2017.
+//  Copyright © 2017 leejaesung. All rights reserved.
+//
+
+import UIKit
+
+class CHUserInfoEditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
